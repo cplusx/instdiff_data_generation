f8f72d490efc18a888c8b2bccbf7e66d17c8e3d9
Ubuntu <ubuntu@ip-172-31-33-50.ec2.internal>
2024-02-25 11:49:33 +0000

    add retrieval script

---
+++ retrieve_model_from_s3.sh
@@ -0,0 +1,2 @@
+aws s3 sync s3://asail-lablet-cv/layout_sdxl/dataset-generation/models/ models/ --exact-timestamps
+aws s3 sync s3://asail-lablet-cv/layout_sdxl/dataset-generation/laion_process_json/ laion_process_json/ --exact-timestamps
