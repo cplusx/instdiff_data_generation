1611d50d185e15efa043370cd7d0f659fccb05a9
Ubuntu <ubuntu@ip-172-31-47-194.ec2.internal>
2024-02-27 04:44:01 +0000

    update code

--- run_generation.sh
+++ run_generation.sh
@@ -1,6 +1,6 @@
-LAION_FOLDER="/home/ubuntu/cjiaxin_16T/LAION400M/laion400m-images"
-folder_idx_start=8
-folder_idx_end=200
+LAION_FOLDER="/home/ubuntu/LAION400M/laion400m-images"
+folder_idx_start=200
+folder_idx_end=400
 
 run_generation() {
     cuda_device=$1
